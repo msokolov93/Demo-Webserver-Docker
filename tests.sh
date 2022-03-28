36475d25ccfb074d7f1b71fe48040d7903d95848
msokolov93 <contact@msokolov.xyz>
2022-03-28 23:14:40 +0000

    returning ssl

--- tests.sh
+++ tests.sh
@@ -27,7 +27,7 @@ if ! [ -x "$(command -v jq)" ]; then
 fi
 
 message " Build image "
-docker build -t agojama/agojama-repo:latest .
+docker build -t mendhak/http-https-echo:latest .
 
 mkdir -p testarea
 pushd testarea
@@ -36,46 +36,46 @@ message " Cleaning up from previous test run "
 docker ps -q --filter "name=http-echo-tests" | grep -q . && docker stop http-echo-tests
 
 message " Start container normally "
-docker run -d --rm --name http-echo-tests -p 8080:8080 -p 8443:8443 -t agojama/agojama-repo
+docker run -d --rm --name http-echo-tests -p 8080:8080 -p 8443:8443 -t mendhak/http-https-echo
 sleep 5
 
 
 message " Make http(s) request, and test the path, method, header and status code. "
-REQUEST=$(curl -s -k -X PUT -H "Arbitrary:Header" -d aaa=bbb 'http://localhost:8443/hello-world?ccc=ddd&myquery=98765')
+REQUEST=$(curl -s -k -X PUT -H "Arbitrary:Header" -d aaa=bbb 'https://localhost:8443/hello-world?ccc=ddd&myquery=98765')
 if [ $(echo $REQUEST | jq -r '.path') == '/hello-world' ] && \
    [ $(echo $REQUEST | jq -r '.method') == 'PUT' ] && \
    [ $(echo $REQUEST | jq -r '.query.myquery') == '98765' ] && \
    [ $(echo $REQUEST | jq -r '.headers.arbitrary') == 'Header' ]
 then
-    passed "HTTP(S) request passed."
+    passed "HTTPS request passed."
 else
-    failed "HTTP(S) request failed."
+    failed "HTTPS request failed."
     echo $REQUEST | jq
     exit 1
 fi
-REQUEST_WITH_STATUS_CODE=$(curl -s -k -o /dev/null -w "%{http_code}" -H "x-set-response-status-code: 404" http://localhost:8443/hello-world)
-REQUEST_WITH_STATUS_CODE_V=$(curl -v -k -o /dev/null -w "%{http_code}" -H "x-set-response-status-code: 404" http://localhost:8443/hello-world)
+REQUEST_WITH_STATUS_CODE=$(curl -s -k -o /dev/null -w "%{http_code}" -H "x-set-response-status-code: 404" https://localhost:8443/hello-world)
+REQUEST_WITH_STATUS_CODE_V=$(curl -v -k -o /dev/null -w "%{http_code}" -H "x-set-response-status-code: 404" https://localhost:8443/hello-world)
 if [ $(echo $REQUEST_WITH_STATUS_CODE == '404') ]
 then
-    passed "HTTP(S) status code header passed."
+    passed "HTTPS status code header passed."
 else
-    failed "HTTP(S) status code header failed."
+    failed "HTTPS status code header failed."
     echo $REQUEST_WITH_STATUS_CODE_V
     exit 1
 fi
 
-REQUEST_WITH_STATUS_CODE=$(curl -s -k -o /dev/null -w "%{http_code}" http://localhost:8443/status/test?x-set-response-status-code=419)
-REQUEST_WITH_STATUS_CODE_V=$(curl -v -k -o /dev/null -w "%{http_code}" http://localhost:8443/hello-world?x-set-response-status-code=419)
+REQUEST_WITH_STATUS_CODE=$(curl -s -k -o /dev/null -w "%{http_code}" https://localhost:8443/status/test?x-set-response-status-code=419)
+REQUEST_WITH_STATUS_CODE_V=$(curl -v -k -o /dev/null -w "%{http_code}" https://localhost:8443/hello-world?x-set-response-status-code=419)
 if [ $(echo $REQUEST_WITH_STATUS_CODE == '419') ]
 then
-    passed "HTTP status code querystring passed."
+    passed "HTTPS status code querystring passed."
 else
-    failed "HTTP status code querystring failed."
+    failed "HTTPS status code querystring failed."
     echo $REQUEST_WITH_STATUS_CODE_V
     exit 1
 fi
 
-REQUEST_WITH_SLEEP_MS=$(curl -o /dev/null -Ss -H "x-set-response-delay-ms: 6000" -k http://localhost:8443/ -w '%{time_total}')
+REQUEST_WITH_SLEEP_MS=$(curl -o /dev/null -Ss -H "x-set-response-delay-ms: 6000" -k https://localhost:8443/ -w '%{time_total}')
 if [[ $(echo "$REQUEST_WITH_SLEEP_MS>5" | bc -l) == 1 ]]; then 
     passed "Request header with response delay passed"
 else 
@@ -84,7 +84,7 @@ else
     exit 1
 fi
 
-REQUEST_WITH_SLEEP_MS=$(curl -o /dev/null -Ss -k http://localhost:8443/sleep/test?x-set-response-delay-ms=5000 -w '%{time_total}')
+REQUEST_WITH_SLEEP_MS=$(curl -o /dev/null -Ss -k https://localhost:8443/sleep/test?x-set-response-delay-ms=5000 -w '%{time_total}')
 if [[ $(echo "$REQUEST_WITH_SLEEP_MS>4" | bc -l) == 1 ]]; then 
     passed "Request query with response delay passed"
 else 
@@ -93,7 +93,7 @@ else
     exit 1
 fi
 
-REQUEST_WITH_INVALID_SLEEP_MS=$(curl -o /dev/null -Ss -H "x-set-response-delay-ms: XXXX" -k http://localhost:8443/ -w '%{time_total}')
+REQUEST_WITH_INVALID_SLEEP_MS=$(curl -o /dev/null -Ss -H "x-set-response-delay-ms: XXXX" -k https://localhost:8443/ -w '%{time_total}')
 if [[ $(echo "$REQUEST_WITH_INVALID_SLEEP_MS<2" | bc -l) == 1 ]]; then 
     passed "Request with invalid response delay passed"
 else 
@@ -129,11 +129,11 @@ message " Stop containers "
 docker stop http-echo-tests
 
 message " Start container with different internal ports "
-docker run -d --rm -e HTTP_PORT=8888 -e HTTP_PORT=9999 --name http-echo-tests -p 8080:8888 -p 8443:9999 -t agojama/agojama-repo
+docker run -d --rm -e HTTP_PORT=8888 -e HTTPS_PORT=9999 --name http-echo-tests -p 8080:8888 -p 8443:9999 -t mendhak/http-https-echo
 sleep 5
 
 message " Make http(s) request, and test the path, method and header. "
-REQUEST=$(curl -s -k -X PUT -H "Arbitrary:Header" -d aaa=bbb http://localhost:8443/hello-world)
+REQUEST=$(curl -s -k -X PUT -H "Arbitrary:Header" -d aaa=bbb https://localhost:8443/hello-world)
 if [ $(echo $REQUEST | jq -r '.path') == '/hello-world' ] && \
    [ $(echo $REQUEST | jq -r '.method') == 'PUT' ] && \
    [ $(echo $REQUEST | jq -r '.headers.arbitrary') == 'Header' ]
@@ -162,7 +162,7 @@ message " Stop containers "
 docker stop http-echo-tests
 
 message " Start container with empty responses "
-docker run -d --rm -e ECHO_BACK_TO_CLIENT=false --name http-echo-tests -p 8080:8080 -p 8443:8443 -t agojama/agojama-repo
+docker run -d --rm -e ECHO_BACK_TO_CLIENT=false --name http-echo-tests -p 8080:8080 -p 8443:8443 -t mendhak/http-https-echo
 sleep 5
 REQUEST=$(curl -s -k http://localhost:8080/a/b/c)
 if [[ -z ${REQUEST} ]]
@@ -178,7 +178,7 @@ message " Stop containers "
 docker stop http-echo-tests
 
 message " Start container with JWT_HEADER "
-docker run -d --rm -e JWT_HEADER=Authentication --name http-echo-tests -p 8080:8080 -p 8443:8443 -t agojama/agojama-repo
+docker run -d --rm -e JWT_HEADER=Authentication --name http-echo-tests -p 8080:8080 -p 8443:8443 -t mendhak/http-https-echo
 sleep 5
 
 REQUEST=$(curl -s -k -H "Authentication: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" https://localhost:8443/ )
@@ -198,9 +198,9 @@ docker stop http-echo-tests
 
 
 message " Start container with LOG_IGNORE_PATH "
-docker run -d --rm -e LOG_IGNORE_PATH=/ping --name http-echo-tests -p 8080:8080 -p 8443:8443 -t agojama/agojama-repo
+docker run -d --rm -e LOG_IGNORE_PATH=/ping --name http-echo-tests -p 8080:8080 -p 8443:8443 -t mendhak/http-https-echo
 sleep 5
-curl -s -k -X POST -d "banana" http://localhost:8443/ping > /dev/null
+curl -s -k -X POST -d "banana" https://localhost:8443/ping > /dev/null
 
 if [ $(docker logs http-echo-tests | wc -l) == 2 ] && \
    ! [ $(docker logs http-echo-tests | grep banana) ]
@@ -217,9 +217,9 @@ message " Stop containers "
 docker stop http-echo-tests
 
 message " Start container with DISABLE_REQUEST_LOGS "
-docker run -d --rm -e DISABLE_REQUEST_LOGS=true --name http-echo-tests -p 8080:8080 -p 8443:8443 -t agojama/agojama-repo
+docker run -d --rm -e DISABLE_REQUEST_LOGS=true --name http-echo-tests -p 8080:8080 -p 8443:8443 -t mendhak/http-https-echo
 sleep 5
-curl -s -k -X GET http://localhost:8443/strawberry > /dev/null
+curl -s -k -X GET https://localhost:8443/strawberry > /dev/null
 if  [ $(docker logs http-echo-tests | grep -c "GET /strawberry HTTP/1.1") -eq 0 ]
 then
     passed "DISABLE_REQUEST_LOGS disabled Express HTTP logging"
@@ -234,9 +234,9 @@ message " Stop containers "
 docker stop http-echo-tests
 
 message " Start container with LOG_WITHOUT_NEWLINE "
-docker run -d --rm -e LOG_WITHOUT_NEWLINE=1 --name http-echo-tests -p 8080:8080 -p 8443:8443 -t agojama/agojama-repo
+docker run -d --rm -e LOG_WITHOUT_NEWLINE=1 --name http-echo-tests -p 8080:8080 -p 8443:8443 -t mendhak/http-https-echo
 sleep 5
-curl -s -k -X POST -d "tiramisu" http://localhost:8443/ > /dev/null
+curl -s -k -X POST -d "tiramisu" https://localhost:8443/ > /dev/null
 
 if [ $(docker logs http-echo-tests | wc -l) == 3 ] && \
    [ $(docker logs http-echo-tests | grep tiramisu) ]
@@ -253,7 +253,7 @@ message " Stop containers "
 docker stop http-echo-tests
 
 message " Check that container is running as a NON ROOT USER by default"
-docker run -d --name http-echo-tests --rm agojama/agojama-repo
+docker run -d --name http-echo-tests --rm mendhak/http-https-echo
 
 WHOAMI=$(docker exec http-echo-tests whoami)
 
@@ -269,9 +269,9 @@ message " Stop containers "
 docker stop http-echo-tests
 
 message " Check that container is running as user different that the user defined in image"
-IMAGE_USER="$(docker image inspect agojama/agojama-repo -f '{{ .ContainerConfig.User }}')"
+IMAGE_USER="$(docker image inspect mendhak/http-https-echo -f '{{ .ContainerConfig.User }}')"
 CONTAINER_USER="$((IMAGE_USER + 1000000))"
-docker run -d --name http-echo-tests --rm -u "${CONTAINER_USER}" -p 8080:8080 agojama/agojama-repo
+docker run -d --name http-echo-tests --rm -u "${CONTAINER_USER}" -p 8080:8080 mendhak/http-https-echo
 sleep 5
 curl -s http://localhost:8080 > /dev/null
 
